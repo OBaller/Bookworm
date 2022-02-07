183ade062f90793d9a423b651d06a2d88905cecd
brightsummit.co <naseemoyebola@gmail.com>
2022-02-07 13:06:18 +0100

    feat: alphabetical sort

--- Bookworm/Bookworm/DetailView.swift
+++ Bookworm/Bookworm/DetailView.swift
@@ -6,26 +6,54 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct DetailView: View {
+    let book: Book
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
 
-struct DetailView_Previews: PreviewProvider {
-    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author ?? "Unknown author")
+                .font(.title)
+                .foregroundColor(.secondary)
 
-    static var previews: some View {
-        let book = Book(context: moc)
-        book.title = "Test book"
-        book.author = "Test author"
-        book.genre = "Fantasy"
-        book.rating = 4
-        book.review = "This was a great book; I really enjoyed it."
+            Text(book.review ?? "No review")
+                .padding()
 
-        return NavigationView {
-            DetailView(book: book)
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
         }
-    }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationBarTitleDisplayMode(.inline)    }
 }
+
+//struct DetailView_Previews: PreviewProvider {
+//    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+//
+//    static var previews: some View {
+//        let book = Book(context: moc)
+//        book.title = "Test book"
+//        book.author = "Test author"
+//        book.genre = "Fantasy"
+//        book.rating = 4
+//        book.review = "This was a great book; I really enjoyed it."
+//
+//        return NavigationView {
+//            DetailView(book: book)
+//        }
+//    }
+//}
